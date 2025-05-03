f52ac2b87424aa320063d643654cfa39e3a46028
Jumabek Namazbekov <m.jumabek@list.ru>
2025-05-03 11:26:16 +0700

    Добавьте файлы проекта.

---
+++ cwitch_cpp/cwitch_cpp.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+	int a;
+	cout << "Введите число: " << endl;
+	cin >> a;
+
+	switch (a)
+	
+	{
+	case 1:
+		cout << "Вы ввели 1" << endl;
+		break;
+	case 2:
+		cout << "Вы ввели 2" << endl;
+		break;
+	default:
+		cout << "Я не знаю этого числа" << endl;
+		break;
+	}
+}
